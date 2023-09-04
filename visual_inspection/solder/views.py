bbf9af0ce1072cc24672e5ac13af1373a9af5938
ikedachin <ikedachin@gmail.com>
2023-09-04 21:57:05 +0900

    小変更

--- visual_inspection/solder/views.py
+++ visual_inspection/solder/views.py
@@ -7,16 +7,17 @@ import shutil
 
 # Create your views here.
 
+#image pathのリストを作っておく
+# image_file_path = glob.glob('.../images/*.jpeg')
+image_path = sorted(glob.glob('./solder/static/raw_images/*.jpeg'))
 
 class Image_path(TemplateView):
     template_name = 'index.html'
     def get_image_list(self, **kywargs):
         context = super().get_context_data(**kywargs)
+        context['num_file'] = len(image_path)
         return context
 
-#image pathのリストを作っておく
-# image_file_path = glob.glob('.../images/*.jpeg')
-image_path = sorted(glob.glob('./solder/static/raw_images/*.jpeg'))
 
 def get_image_path(request, type_name):
     index = int(type_name)
@@ -34,14 +35,18 @@ def get_image_path(request, type_name):
     return JsonResponse(raw_images)
 
 
+import time
+
 def inspction_image(request, type_name):
     print('aaa')
     inspected_dir = './solder/static/inspected_image/'
-    shutil.copy(raw_images[type_name], inspected_dir)
+    shutil.copy(raw_images[type_name], inspected_dir) # ここの代わりにYOLOでの推論を入れる
+
     basename = os.path.basename(type_name)
     inspected_path = inspected_dir + basename
     inspected_row = {os.path.basename(type_name): inspected_path}
     print(inspected_row)
+    time.sleep(1)
     return JsonResponse(inspected_row)
 
 
